787d497dfcfd8a4a08464e5471564a2cde7eaaac
monic <monic@SPC>
2023-02-22 00:23:57 -0600

    Initial commit

---
+++ BesantTechnology/src/com/test/Sample.java
@@ -0,0 +1,19 @@
+package com.test;
+
+public class Sample {
+
+	public void sampleId() {
+		System.out.println("Hello Java");
+	}
+
+	private void sampleNo() {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World");
+	}
+
+	public static void main(String[] args) {
+		Sample s = new Sample();
+s.sampleId();
+s.sampleNo();
+	}
+}
